8c1bc3ff2f5a5189c06daf118a084e44fab90d1e
llikian <pokillian64@gmail.com>
2024-09-28 16:49:58 +0200

    Cleaned up Terrain struct

--- include/Terrain.hpp
+++ include/Terrain.hpp
@@ -14,15 +14,22 @@ using namespace glm;
  * @brief Contains all the data needed to render the terrain.
  */
 struct Terrain {
-    float chunkSize{8.0f};
-    int chunks{200};
-    float tesselationFactor = {8.0f};
+    Terrain() :
+        chunkSize(8.0f),
+        chunks(200),
+        tesselationFactor(8.0f),
+        weights(0.0f, 0.2f, 0.5f, 1.0f),
+        colors(
+            vec3(0.184f, 0.694f, 0.831f),
+            vec3(0.357f, 0.6f, 0.369f),
+            vec3(0.58f, 0.49f, 0.388f),
+            vec3(0.969f, 1.0f, 0.996f)
+        ) { }
 
-    float weights[4]{0.0f, 0.2f, 0.5f, 1.0f};
-    vec3 colors[4]{
-        vec3(0.184f, 0.694f, 0.831f),
-        vec3(0.357f, 0.6f, 0.369f),
-        vec3(0.58f, 0.49f, 0.388f),
-        vec3(0.969f, 1.0f, 0.996f)
-    };
+    float chunkSize;
+    int chunks;
+    float tesselationFactor;
+
+    float weights[4];
+    vec3 colors[4];
 };
