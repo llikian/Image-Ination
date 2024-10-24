039a89c421996b44f2389a0b6c445c3b80143e43
llikian <pokillian64@gmail.com>
2024-10-24 11:18:37 +0200

    Removed some (now) unused variables

--- include/Terrain.hpp
+++ include/Terrain.hpp
@@ -17,27 +17,10 @@ struct Terrain {
     Terrain() :
         chunkSize(32.0f),
         chunks(128),
-        weights(0.0f, 0.2f, 0.5f, 1.0f),
-        colors(vec3(0.184f, 0.694f, 0.831f),
-               vec3(0.357f, 0.6f, 0.369f),
-               vec3(0.58f, 0.49f, 0.388f),
-               vec3(0.969f, 1.0f, 0.996f)),
-        seed(0),
-        freqAnoise(0.01f), ampAnoise(100.0f), octAnoise(1), seedAnoise(789),
         isFogActive(true) { }
 
     const float chunkSize;
     const int chunks;
-
-    float weights[4];
-    vec3 colors[4];
-
-    int seed;
-
-    float freqAnoise;
-    float ampAnoise;
-    int octAnoise;
-    int seedAnoise;
 
     bool isFogActive;
 };
